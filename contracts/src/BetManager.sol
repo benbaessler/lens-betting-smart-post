011efa7d51ef913e063a437d2371554534665ae3
benbaessler <mail@benbaessler.com>
2023-11-24 15:45:04 +0000

    feat(contracts): stake function

--- contracts/src/BetManager.sol
+++ contracts/src/BetManager.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "./interfaces/IERC20.sol";
+import {ILensHub} from "./interfaces/ILensHub.sol";
 
 contract BetManager {
     /// @param creatorId: profile id of the bet creator
@@ -21,6 +22,9 @@ contract BetManager {
         bool decided;
         bool outcome;
     }
+
+    ILensHub public lensHub =
+        ILensHub(0xC1E77eE73403B8a7478884915aA599932A677870);
 
     mapping(uint256 profileId => mapping(uint256 pubId => Bet)) private bets;
 
@@ -51,5 +55,45 @@ contract BetManager {
             false,
             false
         );
+    }
+
+    /// @notice participants stake tokens to activate the bet
+    /// @param staker: 0 for creator, 1 for challenged user
+    function stake(
+        uint256 pubId,
+        uint256 profileId,
+        uint256 staker
+    ) external payable {
+        require(
+            staker < 3,
+            "Invalid staker value: must be 0 for creator or 1 for challenged user"
+        );
+
+        Bet storage bet = bets[profileId][pubId];
+        require(!bet.active, "Bet is already active");
+        require(!bet.decided, "Bet is already completed");
+        require(
+            bet.deadline > block.timestamp,
+            "The deadline has already passed"
+        );
+        require(msg.value == bet.amount, "Incorrect amount");
+
+        if (staker == 0) {
+            require(
+                lensHub.ownerOf(bet.creatorId) == msg.sender,
+                "You are not allowed to stake for the creator"
+            );
+            bet.creatorStaked = true;
+        } else if (staker == 1) {
+            require(
+                lensHub.ownerOf(bet.userId) == msg.sender,
+                "You are not allowed to stake for the challenged user"
+            );
+            bet.userStaked = true;
+        }
+
+        if (bet.creatorStaked && bet.userStaked) {
+            bet.active = true;
+        }
     }
 }
