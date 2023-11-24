b4f5f821d33f1c3f6756f3d65e1865e771ebeb38
benbaessler <mail@benbaessler.com>
2023-11-24 22:26:24 +0000

    refactor(contracts): run script

--- contracts/script/BetManager.s.sol
+++ contracts/script/BetManager.s.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {HelloWorldOpenAction} from "src/HelloWorldOpenAction.sol";
-import {HelloWorld} from "src/HelloWorld.sol";
-import {IHelloWorld} from "src/interfaces/IHelloWorld.sol";
+import {IBetManager} from "src/interfaces/IBetManager.sol";
+import {BetManager} from "src/BetManager.sol";
+import {BetOpenAction} from "src/BetOpenAction.sol";
 
-contract HelloWorldScript is Script {
+contract BetManagerScript is Script {
     function setUp() public {}
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        IHelloWorld helloWorld = new HelloWorld();
+        IBetManager betManager = new BetManager();
         address lensHubProxyAddress = 0xC1E77eE73403B8a7478884915aA599932A677870;
 
-        new HelloWorldOpenAction(lensHubProxyAddress, address(helloWorld));
+        new BetOpenAction(lensHubProxyAddress, address(betManager));
 
         vm.stopBroadcast();
     }
