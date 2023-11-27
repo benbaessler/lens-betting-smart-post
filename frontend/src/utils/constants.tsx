52170235bb61ecbed67ef604491858a330506047
benbaessler <mail@benbaessler.com>
2023-11-27 11:16:53 +0000

    chore(frontend): update constants

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -1,10 +1,16 @@
-export const helloWorldContractAddress =
-  "0xEcfeeE4dcEa32f109da4Ad4D97453cC2d998B60A";
-export const helloWorldContractStartBlock = 41812097;
-export const openActionContractAddress =
-  "0xfd2F3677147047F327FA5506D94D54d93080C7D9";
-export const openActionsContractStartBlock = 41812097;
+export const betManagerContractAddress =
+  "0x84885f10Fcca4C56e66F25bF3fA2048022A80681";
+export const betManagerContractStartBlock = 42898785;
 
-export const lensHubProxyAddress = "0xC1E77eE73403B8a7478884915aA599932A677870";
+export const openActionContractAddress =
+  "0x78240b65F1a4C946DBeA6064a6e8E0AA72d4629C";
+export const openActionsContractStartBlock = 42898785;
+
+// Delete later
+export const helloWorldContractAddress =
+  "0xE7BA30b85D732F728935fd0d476F223b8A610664";
+export const helloWorldContractStartBlock = 42789905;
+
+export const lensHubProxyAddress = "0x4fbffF20302F3326B20052ab9C217C44F6480900";
 export const blockExplorerLink = "https://mumbai.polygonscan.com/tx/";
 export const rpc = "https://rpc.ankr.com/polygon_mumbai";
