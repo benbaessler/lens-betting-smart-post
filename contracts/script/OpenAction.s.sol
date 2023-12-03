0012281d063b76abdb40ae3918c653ad3511b7b3
benbaessler <mail@benbaessler.com>
2023-12-03 13:38:18 +0000

    chore(contracts): update run script

--- contracts/script/OpenAction.s.sol
+++ contracts/script/OpenAction.s.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {IBetManager} from "src/interfaces/IBetManager.sol";
-import {BetManager} from "src/BetManager.sol";
 import {BetOpenAction} from "src/BetOpenAction.sol";
 
-contract BetManagerScript is Script {
+contract OpenActionScript is Script {
     function setUp() public {}
 
     function run() external {
@@ -14,9 +12,9 @@ contract BetManagerScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         address lensHubProxyAddress = 0x4fbffF20302F3326B20052ab9C217C44F6480900;
-        IBetManager betManager = new BetManager(lensHubProxyAddress);
+        address moduleRegistryAddress = 0x4BeB63842BB800A1Da77a62F2c74dE3CA39AF7C0;
 
-        new BetOpenAction(lensHubProxyAddress, address(betManager));
+        new BetOpenAction(lensHubProxyAddress, moduleRegistryAddress);
 
         vm.stopBroadcast();
     }
