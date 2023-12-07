44619e96d1998ddbb74a11659ea8f28c43173006
benbaessler <mail@benbaessler.com>
2023-12-07 14:15:34 +0000

    chore(frontend): update utils

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const numberToHex = (num: number): string => {
+  let hexValue = num.toString(16);
+
+  // Ensure an even number of digits
+  if (hexValue.length % 2 !== 0) {
+    hexValue = "0" + hexValue;
+  }
+
+  return `0x${hexValue}`;
+};
