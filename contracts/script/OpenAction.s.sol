0af56a0bb97d3fcf1ba0d6aaf94b1e6a3717faab
benbaessler <mail@benbaessler.com>
2024-01-07 14:34:17 +0100

    chore(contracts): update deployment script

--- contracts/script/OpenAction.s.sol
+++ contracts/script/OpenAction.s.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {BetOpenAction} from "src/BetOpenAction.sol";
-import {IERC20} from "src/interfaces/IERC20.sol";
 
 contract OpenActionScript is Script {
     function setUp() public {}
@@ -17,16 +16,10 @@ contract OpenActionScript is Script {
 
         address devWallet = 0xa85B5383e0E82dBa993747834f91FE03FCCD40ab;
 
-        BetOpenAction openAction = new BetOpenAction(
+        new BetOpenAction(  
             lensHubProxyAddress,
             moduleRegistryAddress,
             devWallet
-        );
-
-        // Approve WMATIC spending for BetOpenAction
-        IERC20(0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889).approve(
-            address(openAction),
-            type(uint256).max
         );
 
         vm.stopBroadcast();
